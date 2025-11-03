9b1124fb27e4c84ce0055648ee37caea68672e68
Armstrong Opondo Omuhinda <ARMSTRONGOPONDO>
2025-11-03 23:18:30 +0300

    Fix: Add Railway domains to ALLOWED_HOSTS and collectstatic command

--- ecomply/settings.py
+++ ecomply/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=9i9sv-!m4j-7t2f7vm)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.railway.app,*.up.railway.app').split(',')
 
 
 # Application definition
